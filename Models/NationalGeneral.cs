4a9fafe246243c2c3433e5e0d90feb00e28f9425
Liam Cammpbell <liam22campbell@gmail.com>
2023-02-12 19:50:33 -0800

    camelCase answerArray

--- Models/NationalGeneral.cs
+++ Models/NationalGeneral.cs
@@ -2,25 +2,25 @@ namespace CodingChallenge
 {
   class Solution {
     public int[] solution(int N) {
-        int[]AnswerArray = new int [N];
+        int[]answerArray = new int [N];
         if(N ==1){
-            return AnswerArray;
+            return answerArray;
         }
         for(int i = 0; i < N; i += 2){
             if(N % 2 == 1 && i == 0){
-                AnswerArray[0] = 0;
-                AnswerArray[1] = -1;
-                AnswerArray[2] = 1;
+                answerArray[0] = 0;
+                answerArray[1] = -1;
+                answerArray[2] = 1;
                 i += 1;
             } else if ( i == 0 ){
-                AnswerArray[i] = -N;
-                AnswerArray[i + 1] = N;
+                answerArray[i] = -N;
+                answerArray[i + 1] = N;
             } else {
-                AnswerArray[i] = -i;
-                AnswerArray[i + 1] = i;
+                answerArray[i] = -i;
+                answerArray[i + 1] = i;
             }
         }
-        return AnswerArray;
+        return answerArray;
     }
   }
 }
